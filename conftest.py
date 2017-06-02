780b4f2dc73886483657e4f0816cdf7de39bfbe0
Jopika <jojo12345678@gmail.com>
2017-06-02 07:36:23 -0400

    [WIP] Add new ways to try testing

--- conftest.py
+++ conftest.py
@@ -5,32 +5,39 @@ import bot.handler as handler
 import bot.permissions as permissions
 import bot.commands as commands
 
-SERVER_ID = 152522880733675522
-CHANNEL_ID = 319657072792829954
-USER_ID = 319258867089539074
+SERVER_ID = "152522880733675522"
+CHANNEL_ID = "319657072792829954"
+USER_ID = "319258867089539074"
+
+
+# @pytest.fixture(scope="session", autouse=True)
+# def init(request):
+#     global token
+#     global owner_id
+#     global adminList
+#     global client
+#     global whitelist_roles
+#     global roleList
+#     global whitelist_commands
+#     global commandList
+#     handler.init("./test_config.yaml")
+#     token = handler.token
+#     owner_id = handler.owner_id
+#     adminList = handler.adminList
+#     client = handler.client
+#     whitelist_roles = handler.whitelist_roles
+#     roleList = handler.roleList
+#     whitelist_commands = handler.whitelist_commands
+#     commandList = handler.commandList
+#     request.addfinalizer(client.close)
 
 
 @pytest.fixture(scope="session", autouse=True)
-def init(request):
-    global token
-    global owner_id
-    global adminList
-    global client
-    global whitelist_roles
-    global roleList
-    global whitelist_commands
-    global commandList
+async def init():
     handler.init("./test_config.yaml")
-    token = handler.token
-    owner_id = handler.owner_id
-    adminList = handler.adminList
-    client = handler.client
-    whitelist_roles = handler.whitelist_roles
-    roleList = handler.roleList
-    whitelist_commands = handler.whitelist_commands
-    commandList = handler.commandList
-    request.addfinalizer(client.close)
-
+    print(handler.token)
+    await handler.client.run(handler.token)
+    # request.addfinalizer(handler.client.close)
 
 
 @pytest.fixture(scope="session")
@@ -44,20 +51,19 @@ def channel_id():
 
 
 @pytest.fixture(scope="session")
-def client():
-    return client
-
-
-@pytest.fixture(scope="session")
-def server(client):
-    return client.get_server(SERVER_ID)
-
-
-@pytest.fixture(scope="session")
-def channel(client):
-    return client.get_server(SERVER_ID).get_channel(CHANNEL_ID)
-
-
-@pytest.fixture(scope="session")
 def user_id():
     return USER_ID
+
+# @pytest.fixture(scope="session")
+# def client():
+#     return handler.client
+#
+#
+# @pytest.fixture(scope="session")
+# def server(client):
+#     return handler.client.get_server(SERVER_ID)
+#
+#
+# @pytest.fixture(scope="session")
+# def channel(client):
+#     return handler.client.get_server(SERVER_ID).get_channel(CHANNEL_ID)
--- test/test_commands.py
+++ test/test_commands.py
@@ -4,6 +4,7 @@ import discordBot
 import bot.handler as handler
 import bot.permissions as permissions
 import bot.commands as commands
+import time
 
 """This test bank verifies that commands are functioning correctly"""
 
@@ -11,12 +12,31 @@ import bot.commands as commands
 class TestAddRole(object):
     """Tests the functionality of the !addRole funcitons"""
 
-    def test_single_one(self, client, server, channel, user_id):
-        roles = server.get_member(user_id).roles
+    def test_single_one(self, server_id, channel_id, user_id):
+        client = handler.client
+        # counter = 0
+        # while not client._is_ready._value and counter < 10:
+        #     time.sleep(1)
+        #     counter += 1
+        server = client.get_server(server_id)
+        channel = client.get_channel(channel_id)
+        print(type(client))
         user = server.get_member(user_id)
-        if len(roles) > 1:
-            for role in roles[1:]:
-                client.remove_roles(user, role)
-        assert (user.roles == 1)
+        # user_roles = user.roles
+        # assert len(user_roles) == 1
+        assert 1 == 1
+
+
+        # server = handler.client.get_server()
+        # test_user_roles = handler.client.get_channel(test.CHANNEL_ID).get_user(test.USER_ID)
+
+
+
+        # roles = server.get_member(user_id).roles
+        # user = server.get_member(user_id)
+        # if len(roles) > 1:
+        #     for role in roles[1:]:
+        #         client.remove_roles(user, role)
+        # assert (user.roles == 1)
 
 
